bcee4adb6fc89a9ae10532b1f4554124f0d7ad43
vaishnaviarora <123967735+vaishnaviarora@users.noreply.github.com>
2023-05-29 15:24:18 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import Board from './components/Board';
+import Keyboard from './components/Keyboard';
+import {createContext,useState } from "react";
+import { boardDefault } from './Words'
+
+export const AppContext = createContext();
+
+function App() {
+  const [board, setBoard] =  useState(boardDefault);
+  const [currAttempt, setCurrAttempt] = useState({attempt:0, letterPos:0})
+  return (
+    <div className="App">
+      <nav>
+        <h1>Wordle</h1>
+      </nav>
+      <AppContext.Provider value={{board, setBoard,currAttempt, setCurrAttempt}}>
+        <div className='game'>
+          <Board />
+          <Keyboard />
+        </div>
+        
+      </AppContext.Provider>
+     
+    </div>
+  );
+}
+
+export default App;
